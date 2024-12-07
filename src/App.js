5ba7db9c2fa14a48ed285e8fb9b8a2238992cb5e
Rnbsov <rnbsov@gmail.com>
2024-12-07 10:43:57 +0500

    🎉 feat: Update content of `App.js`
    Changelog: feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
+import { html } from 'htm/react'
+
 export default ({ app }) => {
   return html`
-  <${Grid} container spacing=${2}>
-    <${Grid} item xs=${12}>
-      <${ControlPanel} />
-    </>
-    <${Grid} item xs=${6}>
-      <${PeersPanel} />
-    </>
-    <${Grid} item xs=${6}>
-      <${UserPanel} />
-    </>
-  </>
+  Hello world!
 `
 }
